be6da561b9fa88d44ca344798dbadeb0a3846d7b
bglgwyng <bglgwyng@gmail.com>
2025-07-26 22:04:42 +0900

    refactor: replace hello world example with core module exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export function myFunction() {
-	return "Hello, world!";
-}
+export { State } from "./behavior/State";
+export { DerivedEvent } from "./event/DerivedEvent";
+export { Event } from "./event/Event";
+export { Source } from "./event/Source";
+export { Timeline } from "./Timeline";
