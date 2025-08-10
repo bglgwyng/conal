d35e77de1bcc2e4ffde3991334fdb24e4b07cb7c
bglgwyng <bglgwyng@gmail.com>
2025-08-10 19:34:58 +0900

    refactor: export all members from globalContext and Timeline modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./Dynamic";
 export * from "./Event";
 export * from "./factory";
-export { getActiveTimeline, useTimeline, withTimeline } from "./globalContext";
-export { Timeline } from "./Timeline";
+export * from "./globalContext";
+export * from "./Timeline";
