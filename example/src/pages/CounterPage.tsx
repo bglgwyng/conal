77b74a014015632b9ade44913a594a1935789ee9
bglgwyng <bglgwyng@gmail.com>
2025-08-11 15:53:07 +0900

    refactor: move Counter component logic directly into CounterPage

--- example/src/pages/CounterPage.tsx
+++ example/src/pages/CounterPage.tsx
@@ -1,12 +1,25 @@
-import { Stack, Title } from "@mantine/core";
-import { Counter } from "../examples/Counter";
+import { Button, Stack, Title } from "@mantine/core";
+import type { Dynamic } from "conal";
+import { useDynamic } from "../hooks/useDynamic";
+import { useRefFactory } from "../hooks/useRefFactory";
+import { t } from "../timeline";
 
 export function CounterPage() {
+	const [eClick, onClick] = useRefFactory(() => t.source<unknown>());
+	const dynamic: Dynamic<number> = useRefFactory(() =>
+		t.state<number>(
+			0,
+			eClick.transform<number>(() => dynamic.read() + 1),
+		),
+	);
+
+	const count = useDynamic(dynamic);
+
 	return (
 		<Stack>
 			<Title order={2}>Counter Example</Title>
 			<div className="card">
-				<Counter />
+				<Button onClick={onClick}>{count}</Button>
 			</div>
 		</Stack>
 	);
