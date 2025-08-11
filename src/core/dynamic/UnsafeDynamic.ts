a727b55d5a8b72c2ec3a9d90784f4c29267f1bda
bglgwyng <bglgwyng@gmail.com>
2025-08-11 13:02:14 +0900

    feat: add memoization to `UnsafeDynamic`

--- src/core/dynamic/UnsafeDynamic.ts
+++ src/core/dynamic/UnsafeDynamic.ts
@@ -1,16 +1,34 @@
 import type { Timeline } from "../../Timeline";
+import { just, type Maybe } from "../../utils/Maybe";
 import type { Event } from "../event/Event";
 import type { ComputedDynamic } from "./ComputedDynamic";
 import { Dynamic } from "./Dynamic";
 
 export class UnsafeDynamic<T> extends Dynamic<T> {
-	dependedDynamics: Set<ComputedDynamic<any>> = new Set();
+	memoized: Maybe<T>;
+	#read: () => T;
 
 	constructor(
 		timline: Timeline,
-		readonly readCurrent: () => T,
+		read: () => T,
 		readonly updated: Event<T>,
 	) {
 		super(timline);
+		this.#read = read;
+	}
+
+	readCurrent(): T {
+		if (this.memoized) return this.memoized();
+
+		const current = this.#read();
+		this.memoized = just(current);
+
+		this.timeline.needCommit(this);
+
+		return current;
+	}
+
+	commit() {
+		this.memoized = undefined;
 	}
 }
